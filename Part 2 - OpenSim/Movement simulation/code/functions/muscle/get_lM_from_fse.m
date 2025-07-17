83ff05acf289488327bf70b8f354e8854cefc780
Tim <timvdzee@hotmail.com>
2025-07-18 01:22:39 +0200

    undo some changes

--- Part 2 - OpenSim/Movement simulation/code/functions/muscle/get_lM_from_fse.m
+++ Part 2 - OpenSim/Movement simulation/code/functions/muscle/get_lM_from_fse.m
@@ -1,4 +1,4 @@
-function[lM, lMtilda, cos_alpha, dL] = get_lM_from_fse(fse, lMT, params, kT)
+function[lM, lMtilda, cos_alpha] = get_lM_from_fse(fse, lMT, params, kT)
 
 lMo = params(2);
 lTs = params(3);
@@ -6,14 +6,12 @@ alphao = params(4);
 
 lTtilda = fse/kT + 1;
 
-dL = lMT-lTs.*lTtilda;
-
-lM = sqrt((lMo.*sin(alphao)).^2+(lMT-lTs.*lTtilda).^2); % [m]
-cos_alpha = (lMT-lTs.*lTtilda)./lM;
+% lM = sqrt((lMo.*sin(alphao)).^2+(lMT-lTs.*lTtilda).^2); % [m]
+% cos_alpha = (lMT-lTs.*lTtilda)./lM;
 
 % ignoring pennation
-% lM = (lMT-lTs.*lTtilda);
-% cos_alpha = 1;
+lM = (lMT-lTs.*lTtilda);
+cos_alpha = 1;
 
 lMtilda = lM./lMo; % []
 
